c9f6c1d8e56fda0a3fb54267d777f009a75e6150
Ann-MarieKemp <aedalenb@gmail.com>
2020-07-20 15:23:55 -0400

    feat: render table and buttons with api info

--- src/pages/hooks/useAPI.js
+++ src/pages/hooks/useAPI.js
@@ -1,5 +1,26 @@
 import { graphql, useStaticQuery } from "gatsby"
 
-// const useAPI = () => {
-//   const data = useStaticQuery(graphql``)
-// }
+const useAPI = () => {
+  const data = useStaticQuery(graphql`
+    {
+      allCustomApi {
+        nodes {
+          creditorName
+          firstName
+          lastName
+          minPaymentPercentage
+          balance
+        }
+      }
+    }
+  `)
+  return data.allCustomApi.nodes.map(debt => ({
+    creditorName: debt.creditorName,
+    firstName: debt.firstName,
+    lastName: debt.lastName,
+    minPaymentPercentage: debt.minPaymentPercentage,
+    balance: debt.balance,
+  }))
+}
+
+export default useAPI
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,59 @@
 import React from "react"
+import useAPI from "./hooks/useAPI"
+import styles from "./styles/index.modules.css"
 
 export default function Home() {
+  const debts = useAPI()
+  console.log(debts)
   return (
     <>
-      <h1></h1>
+      <table>
+        <thead>
+          <tr>
+            <th>
+              <input type="checkbox" />
+            </th>
+            <th>Creditor</th>
+            <th>First Name</th>
+            <th>LastName</th>
+            <th>Min Pay%</th>
+            <th>Balance</th>
+          </tr>
+        </thead>
+        <tbody>
+          {debts.map(debt => {
+            const {
+              firstName,
+              lastName,
+              creditorName,
+              minPaymentPercentage,
+              balance,
+            } = debt
+            return (
+              <>
+                <tr>
+                  <td>
+                    <input
+                      className="checkbox"
+                      type="checkbox"
+                      name="checked"
+                    />
+                  </td>
+                  <td>{creditorName}</td>
+                  <td>{firstName}</td>
+                  <td>{lastName}</td>
+                  <td>{minPaymentPercentage}</td>
+                  <td>{balance}</td>
+                </tr>
+              </>
+            )
+          })}
+        </tbody>
+      </table>
+      <div className="button-box">
+        <button>Add Debt</button>
+        <button>Remove Debt</button>
+      </div>
     </>
   )
 }
